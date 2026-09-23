34cf4ff335f4561a447f2f7858e545df87c17731
unknown <farhanalibwn1215@gmail.com>
2026-09-23 20:01:43 +0500

    level 1

--- Number_Guess.py
+++ Number_Guess.py
@@ -14,17 +14,22 @@ print(storednum)
 askguess = int(input("Enter Your guess :"))
 
 while askguess != storednum:
-   
-    if askguess > storednum:
+
+    if askguess > 100 or askguess < 0:
+        print("Please enter the number between 0 and 100")
+        attempts += 1
+        guesses -= 1
+
+    elif askguess > storednum:
         print("Guess high!")
 
     elif askguess < storednum:
         print("Guess low!")
 
     attempts -= 1
+    guesses += 1
     print("remaining attempts are: ", attempts)
     askguess = int(input("Enter guess again: "))
-    guesses += 1
     if guesses == 10:
         print("Game Over! Attempts Finished")
         print("The secret number is:" ,storednum)
