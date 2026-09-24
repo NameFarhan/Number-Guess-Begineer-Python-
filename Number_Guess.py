605a7cfaefbaae18b9c03eda876e1e1b3096e31a
unknown <farhanalibwn1215@gmail.com>
2026-09-24 23:43:33 +0500

    L

--- Number_Guess.py
+++ Number_Guess.py
@@ -4,19 +4,22 @@
 
 import random
 
-storednum = random.randint(0,100)
+storednum = random.randint(1,100)
 
 guesses = 1
 attempts = 10
 print("Number guessing game --:--\n")
 print(storednum)
 
-askguess = int(input("Enter guess again: "))
+try:
+    askguess = int(input("Enter your guess: "))
+except ValueError:
+    print("Please enter a valid number!")
 
 while askguess != storednum:
 
     if askguess > 100 or askguess < 0:
-        print("Please enter the number between 0 and 100")
+        print("Please enter the number between 0 and 100!")
         attempts += 1
         guesses -= 1
     elif askguess > storednum:
@@ -28,7 +31,13 @@ while askguess != storednum:
     attempts -= 1
     guesses += 1
     print("remaining attempts are: ", attempts)
-    askguess = int(input("Enter guess again: "))
+    try:
+        askguess = int(input("Enter your guess: "))
+    except ValueError:
+        print("Please enter a valid number!")   
+        attempts += 1
+        guesses -= 1
+     
     if guesses == 10:
         print("Game Over! Attempts Finished")
         print("The secret number is:" ,storednum)
